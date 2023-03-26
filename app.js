7e4f203809b8e34018b24c2a15c81686610c5cdd
Joey <zhaojin.li@uq.net.au>
2023-03-26 11:02:46 +1000

    second commit

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const lodash = require("lodash");
+const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,48 +17,68 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const arr = [];
+mongoose.connect("mongodb://127.0.0.1:27017/blogDB", {useNewUrlParser: true});
 
-app.get("/", function(req, res){
-  res.render("home", {context: homeStartingContent, arr: arr});
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+
+
+app.get("/",async function(req, res){
+  Post.deleteMany({});
+  const post = await Post.find({});
+  res.render("home", {
+    startingContent: homeStartingContent,
+    posts: post
+    });
 });
 
-app.get("/about", function(req, res){
-  res.render("about", {context: aboutContent});
-});
-
-app.get("/contact", function(req, res){
-  res.render("contact", {context: contactContent});
-});
 
 app.get("/compose", function(req, res){
   res.render("compose");
 });
 
-app.get("/:p", function(req, res){
-  arr.forEach(function(element){
-    if (lodash.lowerCase(req.params.p) == lodash.lowerCase(element.title)){
-      res.render("post", {post: element});
-    }
-  })
+app.post("/compose", function(req, res){
+
+  const post = new Post ({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+
+  post.save();
+
+  res.redirect("/");
+
 });
 
-app.post("/compose", function(req, res){
-  let title = req.body.Title;
-  let text = req.body.Post;
-  const post = {title: title, text: text};
-  arr.push(post);
-  console.log(arr);
-  res.redirect("/");
-})
+app.get("/:postName", async function(req, res){
+  const posts = await Post.find({});
+  
+  const requestedTitle = _.lowerCase(req.params.postName);
+  
+  posts.forEach(function(post){
+    const storedTitle = _.lowerCase(post.title);
+    if (storedTitle === requestedTitle) {
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
+    }
+  });
 
+});
 
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
 
-
-
-
-
-
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
